85d16a52fc0d52c009655ad7c3fac15b80cc0efb
YUTO1013afk <yourentiandao304@gmail.com>
2023-05-09 10:37:14 +0900

    Commit

--- 01/cast.c
+++ 01/cast.c
@@ -10,6 +10,6 @@ result1 = (float)num1 / num2;
 printf("result1: %f\n", result1);
 // float -> int にキャストして代⼊
 result2 = (int)result1;
-rintf("result2: %d\n", result2);
+printf("result2: %d\n", result2);
 return 0;
 }
