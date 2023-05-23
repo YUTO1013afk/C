b43bbdd2bddd0e7b35c5c8bbb045125fc296a306
YUTO1013afk <yourentiandao304@gmail.com>
2023-05-23 12:46:42 +0900

    Commit

--- 05/pconcat.c
+++ 05/pconcat.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
-void fconcat(char[], char[], char[]);
+void pconcat(char[], char[], char[]);
 int main(int argc, char const *argv[]) {
     char str1[] = "Hello, ";
     char str2[] = "World";
     char concat[100]; // 結果を⼊れる配列
-    fconcat(concat, str1, str2);
+    pconcat(concat, str1, str2);
     printf("fconcat : %s\n", concat);
     return 0;
 }
 
-void fconcat(char result[], char str1[], char str2[]) {
+void pconcat(char *result, char *str1, char *str2) {
     // 動作するように書き換える
     char *p = result;       // 結果の配列のポインタ
     while (*str1 != '\0') { // str1の終端に達するまで繰り返す
