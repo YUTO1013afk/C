1bfc3a3295f87b2ce34f8d3fd932ce56c94085d2
YUTO1013afk <yourentiandao304@gmail.com>
2023-05-23 12:14:21 +0900

    Commit

---
+++ 05/address2.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main(void) {
+char str[100];
+printf("変数strのアドレスは%p\n", str);
+printf("変数str[0]のアドレスは%p\n", &str[0]);
+}
---
+++ 05/pointer.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(int argc, char const *argv[]) {
+int a = 10;
+int *p = NULL;
+printf("aの値:%d\n", a);
+p = &a;
+printf("pの指し⽰す場所の値:%d\n", *p);
+return 0;
+}
