75cb98a72c18b9b1b351fa9e7643151fae9abdef
YUTO1013afk <yourentiandao304@gmail.com>
2023-05-23 11:13:11 +0900

    Commit

--- prog1.c
+++ prog1.c
@@ -2,17 +2,17 @@
 #include <string.h>
 // reverse関数の定義
 void reverse(char *str) {
-  // ⽂字列の⻑さを求める
-  int len = strlen(str);
-  // 後ろから順に⽂字を出⼒する
-  for (int i = len - 1; i >= 0; i--) {
-    printf("%c", str[i]);
-  }
-  printf("\n");
+    // ⽂字列の⻑さを求める
+    int len = strlen(str);
+    // 後ろから順に⽂字を出⼒する
+    for (int i = len - 1; i >= 0; i--) {
+        printf("%c", str[i]);
+    }
+    printf("\n");
 }
 
 int main(int argc, char const *argv[]) {
-  // reverse関数を呼び出す
-  reverse("Hello, World!");
-  return 0;
+    // reverse関数を呼び出す
+    reverse("Hello, World!");
+    return 0;
 }
--- prog2.c
+++ prog2.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
 // lower関数の定義
 void lower(char *str) {
-  // ⽂字列の各要素をループで処理する
-  int i = 0;
-  while (str[i] != '\0') {
-    // ⼤⽂字の場合は⼩⽂字に変換する
-    if (str[i] >= 'A' && str[i] <= 'Z') {
-      str[i] = str[i] + 32;
+    // ⽂字列の各要素をループで処理する
+    int i = 0;
+    while (str[i] != '\0') {
+        // ⼤⽂字の場合は⼩⽂字に変換する
+        if (str[i] >= 'A' && str[i] <= 'Z') {
+            str[i] = str[i] + 32;
+        }
+        i++;
     }
-    i++;
-  }
 }
 
 int main(int argc, char const *argv[]) {
-  // ⽂字列を定義する
-  char str[] = "HELLO, WORLD";
-  // lower関数を呼び出す
-  lower(str);
-  // ⽂字列を出⼒する
-  printf("%s\n", str);
-  return 0;
+    // ⽂字列を定義する
+    char str[] = "HELLO, WORLD";
+    // lower関数を呼び出す
+    lower(str);
+    // ⽂字列を出⼒する
+    printf("%s\n", str);
+    return 0;
 }
